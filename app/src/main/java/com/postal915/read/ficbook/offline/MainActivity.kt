7ddd47ced26562368955c824f1f03797a36d8f0e
Stas Latyshev <postal915@gmail.com>
2020-10-21 20:38:25 +0300

    Initial commit

---
+++ app/src/main/java/com/postal915/read/ficbook/offline/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.postal915.read.ficbook.offline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
